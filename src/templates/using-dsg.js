bb887a973842f21cc4b585a8c24fe1de4e9890d2
Srilal Sachintha <SrilalSachintha@Gmail.com>
2021-12-09 15:31:05 +0530

    Update using-dsg.js

--- src/templates/using-dsg.js
+++ src/templates/using-dsg.js
@@ -6,8 +6,7 @@ import Seo from "../components/seo"
 
 const UsingDSG = () => (
   <Layout>
-    <Seo title="Using DSG" />
-    <h1>Hello from a DSG Page</h1>
+    <h1>Hello from a DSGX1500 Page</h1>
     <p>This page is not created until requested by a user.</p>
     <p>
       To learn more, head over to our{" "}
